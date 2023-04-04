a731fa896fc2e19d1ff7e1ba84f929221b04a267
Rod <rod.mckay49@gmail.com>
2023-04-04 15:30:07 -0600

    working on TestGithubHistory - it works - can debug into class

--- AddOns/ClassForTestGithubHistory.cs
+++ AddOns/ClassForTestGithubHistory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using NinjaTrader.Code;
 using NinjaTrader.NinjaScript;
 
 namespace NinjaTrader.Custom.AddOns
@@ -11,9 +11,9 @@ namespace NinjaTrader.Custom.AddOns
     public class ClassForTestGithubHistory
     {
 		public void Display()
-		{
-//			NinjaTrader.NinjaScript.ClearOutputWindow();
-			NinjaTrader.Code.Output.Process("my message", PrintTo.OutputTab1);
+        {
+//            NinjaTrader.Code.Output.Reset(PrintTo.OutputTab1);
+            NinjaTrader.Code.Output.Process("my message", PrintTo.OutputTab1);
 		}
     }
 }
